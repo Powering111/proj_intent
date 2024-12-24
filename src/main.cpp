c404d3648a132bf25ecbfe33e79d1efd7eb71cbd
Powering111 <50047351+Powering111@users.noreply.github.com>
2024-12-24 17:06:37 +0900

    Basic rendering

--- src/main.cpp
+++ src/main.cpp
@@ -25,8 +25,12 @@ For a C++ project simply rename the file to .cpp and re-run the build script
 */
 
 #include "raylib.h"
+#include "raymath.h"
 
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+#include "resource_dir.h"
+#include <deque>
+#include <vector>
+#include <cstdint>
 
 int main ()
 {
@@ -38,7 +42,8 @@ int main ()
 
 	SearchAndSetResourceDir("resources");
 
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
+	Texture t_player = LoadTexture("player.png");
+	Texture t_attack = LoadTexture("attack.png");
 	Camera2D camera = { 0 };
 	camera.target = { 0.0f, 0.0f };
 	camera.offset = { 640.0f, 400.0f };
@@ -47,18 +52,49 @@ int main ()
 	camera.zoom = 1.0f;
 
 	Vector2 position = { 0.0f, 0.0f };
+	std::deque<Vector2> trails = std::deque<Vector2>();
+	std::vector<std::pair<Vector2,uint16_t>> attacks = std::vector<std::pair<Vector2,uint16_t>>();
+
+	float player_speed = 5.0f;
+	unsigned int frame = 0;
+	bool clicking = false;
+
+	SetTargetFPS(60);
 	// game loop
 	while (!WindowShouldClose())
 	{
-		if(IsKeyDown(KEY_A)) position.x -= 20.0f;
-		if(IsKeyDown(KEY_D)) position.x += 20.0f;
-		if(IsKeyDown(KEY_W)) position.y -= 20.0f;
-		if(IsKeyDown(KEY_S)) position.y += 20.0f;
-		
+		frame += 1;
+		if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
+			if(!clicking){
+				trails.clear();
+			}
+			trails.push_back(GetScreenToWorld2D(GetMousePosition(), camera));
+			while(trails.size() > 20){
+				trails.pop_front();
+			}
+			clicking = true;
+		}
+		else{
+			if(clicking){
+				attacks.push_back(std::make_pair(GetScreenToWorld2D(GetMousePosition(), camera), 0));
+			}
+			if(!trails.empty()) trails.pop_front();
+			clicking = false;
+		}
+
+		// player move
+		Vector2 next_vector = {0.0f, 0.0f};
+		if(IsKeyDown(KEY_A)) next_vector.x -= 1.0f;
+		if(IsKeyDown(KEY_D)) next_vector.x += 1.0f;
+		if(IsKeyDown(KEY_W)) next_vector.y -= 1.0f;
+		if(IsKeyDown(KEY_S)) next_vector.y += 1.0f;
+		position = Vector2Add(position, Vector2Scale(next_vector, player_speed));
+
 		if(IsKeyDown(KEY_LEFT)) camera.target.x -= 20.0f;
 		if(IsKeyDown(KEY_RIGHT)) camera.target.x += 20.0f;
 		if(IsKeyDown(KEY_UP)) camera.target.y -= 20.0f;
 		if(IsKeyDown(KEY_DOWN)) camera.target.y += 20.0f;
+
 
 		// drawing
 		BeginDrawing();
@@ -66,22 +102,57 @@ int main ()
 		DrawCircle(640, 400, 3.0f, BLUE);
 		DrawCircle(0, 0, 10.0f, GREEN);
 		DrawCircle(1280, 800, 10.0f, GREEN);
+		DrawText(TextFormat("Frame %u", frame), 0, 0, 20, WHITE);
+		DrawText(TextFormat("%d FPS", GetFPS()), 0, 20, 20, WHITE);
 		
 		BeginMode2D(camera);
 
 		DrawCircle(0, 0, 3.0f, RED);
 		DrawCircle(1280, 800, 3.0f, PURPLE);
 		DrawText("HIHI world!!!!", -100,20,30,WHITE);
-		DrawTexture(wabbit, position.x, position.y, WHITE);
-		EndMode2D();
 		
+		// Render player
+		DrawTextureRec(t_player, {64.0f*((frame/10)%2), 0.0f, 64.0f, 128.0f}, {position.x, position.y}, WHITE);
+
+		// Render trail
+		int i = 0;
+		for(std::deque<Vector2>::iterator trail_pos = trails.begin(); trail_pos != trails.end();){
+			Vector2 curr_pos = *trail_pos;
+			float curr_radius = (float)(i+1) / 3.0f;
+			DrawCircle(curr_pos.x, curr_pos.y, curr_radius, {102, 191, 255, 255});
+
+			trail_pos++;
+			if(trail_pos != trails.end()){
+				Vector2 next_pos = *trail_pos;
+				DrawLineEx(curr_pos, next_pos, curr_radius*2.0f, {102, 191, 255, 255});
+			}
+			i++;
+		}
+
+		// Render attacks
+		for(std::vector<std::pair<Vector2, uint16_t>>::iterator attack = attacks.begin(); attack != attacks.end(); ){
+			uint16_t anim_idx = (*attack).second / 2;
+			if(anim_idx > 4){
+				// destroy attack
+				attack = attacks.erase(attack);
+			}
+			else{
+				// DrawTextureRec(t_attack, {64.0f * anim_idx, 0.0f, 64.0f, 64.0f}, Vector2Add((*attack).first, {-32.0f, -32.0f}), WHITE);
+				DrawTexturePro(t_attack, {64.0f * anim_idx, 0.0f, 64.0f, 64.0f}, {(*attack).first.x-64.0f, (*attack).first.y-64.0f, 128.0f, 128.0f}, {0.0f, 0.0f}, 0.0f, WHITE);
+				*attack = std::make_pair((*attack).first, (*attack).second + 1);
+				attack++;
+			}
+		}
+
+		EndMode2D();
+
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
 	}
 
 	// cleanup
 	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
+	UnloadTexture(t_player);
 
 	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
